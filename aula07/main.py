b32cd4da6495cef547e840357aa5661188d3f599
Mateus william aragao castro <Mateuswill11@academicos.com>
2023-04-17 21:08:48 -0300

    inicial aula07/

---
+++ aula07/main.py
@@ -0,0 +1,13 @@
+# declaraçao da lista
+nomes = ["mateus", "caua", "nadine", "savana"]
+# impressao da lista
+print(nomes)
+# impressao do tipo de dados
+print(type(nomes))
+# tamanho da lista
+print(len(nomes))
+# imprimir os iten da lista
+print(nomes[0])
+print(nomes[1])
+print(nomes[2])
+print(nomes[3])
